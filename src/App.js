7496664812e5bb3758a844b2fcfcf6ac1344ef37
Pattarapol <adisus2@MacBook-Air-khxng-Pattarapol.local>
2021-06-24 15:55:55 +0700

    [UPDATE]app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello this is staging
+          Edit <code>src/App.js</code> and save to reload. Hello this is prod
         </p>
         <a
           className="App-link"
