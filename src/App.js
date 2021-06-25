534a1bfcd7d3fabc7b09d2bbe0b33b9722dee61c
Pattarapol <adisus2@MacBook-Air-khxng-Pattarapol.local>
2021-06-25 11:53:53 +0700

    test feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello mtfk
+          Edit <code>src/App.js</code> and save to reload. Hello mtfk this is test
         </p>
         <a
           className="App-link"
