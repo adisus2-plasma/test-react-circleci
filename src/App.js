7da9c3ce2ea9f906ef49901967773c29af7248e8
Pattarapol Sangsunee <pattarapol262@gmail.com>
2021-07-08 16:54:50 +0700

    [TEST]TEST_STAG from env

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello this is staging
+          Edit <code>src/App.js</code> and save to reload. Hello this is staging ${TEST_STAG}
         </p>
         <a
           className="App-link"
