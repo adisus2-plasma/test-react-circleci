9dbd266b7d81b9b72644e5b54610c3349830afc1
Pattarapol Sangsunee <pattarapol262@gmail.com>
2021-07-08 18:53:52 +0700

    [UPDATE].env from dev to stag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello shit .env ${process.env.REACT_APP_DB_CONNECTION_STAG}
+          Edit <code>src/App.js</code> and save to reload. Hello shit .env ${process.env.REACT_APP_DB_CONNECTION}
         </p>
         <a
           className="App-link"
